111152eab60721871c3d0c8e9958fff28b597eb1
Rodrigo Gonçalves <rodrigo.l.m.goncalves@gmail.com>
2026-04-23 16:23:00 +0100

    Implement Bluetooth value reception in FlightC
    Added Bluetooth functionality for receiving values.

--- FlightController/src/FlightC.cpp
+++ FlightController/src/FlightC.cpp
@@ -4,6 +4,51 @@
 
 namespace FlightC {
 
+    //BluetoothSerial SerialBT;
+
+    Controller::Controller() : U(0), D(0), L(0), R(0) {}
+    Controller::~Controller() {}
+
+    void Controller::setTarget(SepValues sv) {
+        target = (int)sv.val1;
+    }
+    
+    void Controller::prog(int DAC_pin) {
+        if (target > val) {
+            val++;
+            dacWrite(DAC_pin, val);
+            Serial.println(val);
+        } else if (target < val) {
+            val--;
+            dacWrite(DAC_pin, val);
+            Serial.println(val);
+        }
+    }
+
+    SepValues Controller::btReceiver() {
+        static SepValues lastValues = {0, 0};  // guarda o último valor
+    
+        if (SerialBT.available()) {
+            String data = SerialBT.readStringUntil('\n');
+            int sep = data.indexOf(',');
+            if (sep > 0) {
+                float v1 = data.substring(0, sep).toFloat();
+                float v2 = data.substring(sep + 1).toFloat();
+                if (v1 != 0 || v2 != 0) {
+                    lastValues.val1 = v1;
+                    lastValues.val2 = v2;
+                }
+            }
+        }
+        return lastValues;  
+    }
+
+} #include "FlightC.h"
+#include <Arduino.h>
+#include "BluetoothSerial.h"
+
+namespace FlightC {
+
     BluetoothSerial SerialBT;
 
     Controller::Controller() : U(0), D(0), L(0), R(0) {}
