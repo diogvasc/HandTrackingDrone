a824643e67f1ef8aecdb985ab0357315bf0da556
Rodrigo Gonçalves <rodrigo.l.m.goncalves@gmail.com>
2026-04-23 16:22:46 +0100

    Refactor FlightC controller and update PWM limits

--- FlightController/lib/FlightC.h
+++ FlightController/lib/FlightC.h
@@ -6,7 +6,7 @@
 
 namespace FlightC {
 
-   //extern BluetoothSerial SerialBT;
+extern BluetoothSerial SerialBT;
 
     struct SepValues {  // depois serão 4
         float val1;
@@ -14,27 +14,22 @@ namespace FlightC {
     };
 
     class Controller {
-    public:
-        Controller();
-        ~Controller();
-
-        int progUp(int PWM_pin);
-        int progDown(int PWM_pin);
-
-        void setVal(int pin, int val);
-
-        SepValues btReceiver();
-
-    private:
-        static const int MAX_PWM = 181;
-        static const int MIN_PWM = 1;
-
-        int U;  
-        int D;
-        int L;  
-        int R;
-        SepValues sepValues; // set[0] para esquerda, set[1] para direita
-    };
+        public:
+            Controller();
+            ~Controller();
+            void prog(int PWM_pin);           // sem SepValues
+            void setTarget(SepValues sv);     // define o target
+            void setVal(int pin, int val);
+            SepValues btReceiver();
+        
+        private:
+            static const int MAX_PWM = 255;
+            static const int MIN_PWM = 0;
+            int val = 0;
+            int target = 0;   // <-- novo
+            int U, D, L, R;
+            SepValues sepValues;
+        };
 } // namespace FlightC
 
 #endif
